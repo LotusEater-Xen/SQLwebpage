d5aa967f5fd294f13264265a026c5d3db5f71058
LotusEater-Xen <132997810+LotusEater-Xen@users.noreply.github.com>
2025-06-27 10:06:50 +0200

    Add files via upload

--- server.js
+++ server.js
@@ -1,10 +1,21 @@
 import express from 'express'
-
+//path and fix path
+import path    from 'path'
+import { fileURLToPath } from 'url';
 import { getThing,  getThings, createThing } from './connector.js'
+//fix path 
+const __filename = fileURLToPath(import.meta.url); // get the resolved path to the file
+const __dirname = path.dirname(__filename); // get the name of the directory
+
 
 const app = express()
+let pagesPath = path.join(__dirname,'pages')
+    
+app.use(express.urlencoded())
 
-app.use(express.json())
+app.get("/",(req, res) =>{ 
+   res.sendFile(`${pagesPath}/index.html`)
+})
 
 app.get("/things", async(req, res) =>{
     const things = await getThings()
@@ -17,10 +28,14 @@ app.get("/things/:id", async(req, res) =>{
     res.send(thing)
 })
 
-app.post("/things", async(req, res) =>{
+app.post("/", async(req, res) =>{
     const { title, contents} = req.body
-    const note = await createThing(title, contents)
-    res.status(201).send(note)
+    const thing = await createThing(title, contents)
+    res.status(201).send(thing)
+})
+
+app.post("/",function(req, res, next){
+    res.status(201).send(req.body)
 })
 
 app.use((err, req, res, next) => {
